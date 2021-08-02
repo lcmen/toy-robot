0447585daa3e98ecc150d0073a968ff4e7475a1d
Lucas Mendelowski <lucas@mendelowski.com>
2021-08-02 21:37:30 +0200

    Implement turns

--- lib/toy_robot/robot.ex
+++ lib/toy_robot/robot.ex
@@ -24,6 +24,48 @@ defmodule ToyRobot.Robot do
     end
   end
 
+  @doc """
+  Turn the robot left
+
+  ## Examples
+
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %Robot{facing: :north}
+    %Robot{facing: :north}
+    iex> robot |> Robot.turn_left
+    %Robot{facing: :west}
+  """
+  def turn_left(%Robot{facing: facing}) do
+    case facing do
+      :east -> %Robot{facing: :north}
+      :west -> %Robot{facing: :south}
+      :north -> %Robot{facing: :west}
+      :south -> %Robot{facing: :east}
+    end
+  end
+
+  @doc """
+  Turn the robot right
+
+  ## Examples
+
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %Robot{facing: :north}
+    %Robot{facing: :north}
+    iex> robot |> Robot.turn_right
+    %Robot{facing: :east}
+  """
+  def turn_right(%Robot{facing: facing}) do
+    case facing do
+      :east -> %Robot{facing: :south}
+      :west -> %Robot{facing: :north}
+      :north -> %Robot{facing: :east}
+      :south -> %Robot{facing: :west}
+    end
+  end
+
   defp move_east(robot) do
     %Robot{east: robot.east + 1}
   end
--- test/toy_robot/robot_test.exs
+++ test/toy_robot/robot_test.exs
@@ -13,6 +13,16 @@ defmodule ToyRobot.RobotTest do
       robot = robot |> Robot.move()
       assert robot.north == 1
     end
+
+    test "it turns left to face west", %{robot: robot} do
+      robot = robot |> Robot.turn_left()
+      assert robot.facing == :west
+    end
+
+    test "it turns right to face east", %{robot: robot} do
+      robot = robot |> Robot.turn_right()
+      assert robot.facing == :east
+    end
   end
 
   describe "when the robot is facing south" do
@@ -23,6 +33,16 @@ defmodule ToyRobot.RobotTest do
     test "it moves one space south", %{robot: robot} do
       robot = robot |> Robot.move()
       assert robot.north == -1
+    end
+
+    test "it turns left to face east", %{robot: robot} do
+      robot = robot |> Robot.turn_left()
+      assert robot.facing == :east
+    end
+
+    test "it turns right to face west", %{robot: robot} do
+      robot = robot |> Robot.turn_right()
+      assert robot.facing == :west
     end
   end
 
@@ -35,6 +55,16 @@ defmodule ToyRobot.RobotTest do
       robot = robot |> Robot.move()
       assert robot.east == 1
     end
+
+    test "it turns left to face north", %{robot: robot} do
+      robot = robot |> Robot.turn_left()
+      assert robot.facing == :north
+    end
+
+    test "it turns right to face south", %{robot: robot} do
+      robot = robot |> Robot.turn_right()
+      assert robot.facing == :south
+    end
   end
 
   describe "when the robot is facing west" do
@@ -45,6 +75,16 @@ defmodule ToyRobot.RobotTest do
     test "it moves one space west", %{robot: robot} do
       robot = robot |> Robot.move()
       assert robot.east == -1
+    end
+
+    test "it turns left to face south", %{robot: robot} do
+      robot = robot |> Robot.turn_left()
+      assert robot.facing == :south
+    end
+
+    test "it turns right to face north", %{robot: robot} do
+      robot = robot |> Robot.turn_right()
+      assert robot.facing == :north
     end
   end
 end
