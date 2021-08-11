a6ed3e6341d949e5af2e8f8c3809bc830f30d11c
Lucas Mendelowski <lucas@mendelowski.com>
2021-08-11 20:05:15 +0200

    Interpret and run commands

---
+++ test/toy_robot/runner_test.exs
@@ -0,0 +1,105 @@
+defmodule ToyRobot.RunnerTest do
+  use ExUnit.Case, async: true
+
+  alias ToyRobot.{Runner, Simulation}
+  import ExUnit.CaptureIO
+
+  doctest ToyRobot.Runner
+
+  test "it handles a valid place command" do
+    %Simulation{robot: robot} = Runner.run([{:place, %{east: 1, north: 2, facing: :north}}])
+
+    assert robot.east == 1
+    assert robot.north == 2
+    assert robot.facing == :north
+  end
+
+  test "it handles an invalid place command" do
+    simulation = Runner.run([{:place, %{east: 10, north: 10, facing: :north}}])
+    assert simulation == nil
+  end
+
+  test "it ignores commands until a valid placement" do
+    %Simulation{robot: robot} =
+      Runner.run([
+        :move,
+        {:place, %{east: 1, north: 2, facing: :north}}
+      ])
+
+    assert robot.east == 1
+    assert robot.north == 2
+    assert robot.facing == :north
+  end
+
+  test "it handles a place + move command" do
+    %Simulation{robot: robot} =
+      Runner.run([
+        {:place, %{east: 1, north: 2, facing: :north}},
+        :move
+      ])
+
+    assert robot.east == 1
+    assert robot.north == 3
+    assert robot.facing == :north
+  end
+
+  test "it handles a place + invalid move command" do
+    %Simulation{robot: robot} =
+      Runner.run([
+        {:place, %{east: 1, north: 4, facing: :north}},
+        :move
+      ])
+
+    assert robot.east == 1
+    assert robot.north == 4
+    assert robot.facing == :north
+  end
+
+  test "it handles a place + turn_left command" do
+    %Simulation{robot: robot} =
+      Runner.run([
+        {:place, %{east: 1, north: 2, facing: :north}},
+        :turn_left
+      ])
+
+    assert robot.east == 1
+    assert robot.north == 2
+    assert robot.facing == :west
+  end
+
+  test "it handles a place + turn_right command" do
+    %Simulation{robot: robot} =
+      Runner.run([
+        {:place, %{east: 1, north: 2, facing: :north}},
+        :turn_right
+      ])
+
+    assert robot.east == 1
+    assert robot.north == 2
+    assert robot.facing == :east
+  end
+
+  test "it handles a place + report command" do
+    output =
+      capture_io(fn ->
+        Runner.run([
+          {:place, %{east: 1, north: 2, facing: :north}},
+          :report
+        ])
+      end)
+
+    assert String.trim(output) == "The robot is at (1,2) and is facing NORTH"
+  end
+
+  test "it handles a place + invalid command" do
+    %Simulation{robot: robot} =
+      Runner.run([
+        {:place, %{east: 1, north: 2, facing: :north}},
+        {:invalid, "EXTERMINATE"}
+      ])
+
+    assert robot.east == 1
+    assert robot.north == 2
+    assert robot.facing == :north
+  end
+end
